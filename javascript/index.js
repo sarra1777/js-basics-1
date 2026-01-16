89ce75cb845f66d8c3abeeda716e730828b2f937
sarra1777 <sarranaddari568@gmail.com>
2026-01-16 23:40:53 +0200

    day 1 : js basics

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+//console.log('hello'); //this is a comment
+//console.log('i am going to make pizza tomorrow !');
+
+//window.alert('this is an alert box !'); 
+//document.getElementById('title').textContent = 'hello world !'; //to change the content of an element
+//document.getElementById('prg').textContent = 'this is a paragraph !';
+
+/* declaration of variable is let x;
+  assignment  is x=5;*/
+
+let x = 100; //declaration 
+console.log(x); //100
+
+let age = 20;
+let price = 10.99;
+console.log(typeof price); // return the type of the variable
+console.log(`you are ${age} years old ! `);
+console.log('the price is ' + price + ' $ ');
+let gpa = 2.1;
+console.log(gpa);
+
+let firstaname = 'sarra';
+let lastname = 'naddari';
+console.log('your name is ' + firstaname + ' and your lastname is ' + lastname);
+let favoritecolor = 'pink';
+console.log('your favorite color is ' + favoritecolor);
+
+let email = 'sarranaddari@gmail.com';
+console.log('your email is ' + email);
+
+let online = false ;
+let forsale = true;
+console.log(typeof online);
+console.log(`is sarra is online : ${online}`);
+
+console.log(`is this car for sale ?: ${forsale}`);
